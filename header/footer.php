75b357af00f58eefd9ed0971b25f4d5f2a9d72d3
rockstarabhijit0 <83265841+rockstarabhijit0@users.noreply.github.com>
2022-12-20 16:51:01 +0530

    YoutuBe

---
+++ header/footer.php
@@ -0,0 +1,95 @@
+<footer class="footer pt-3 mt-3 pb-0">
+
+                 <h3 class="text-center mb-3">Follow us</h3>
+          <div class="social-share text-center">
+            <div class="share-icons relative">
+                                  <a href="https://www.facebook.com/cloudsconvert" class="btn btn-facebook btn-simple rounded social-shadow p-2" target="_blank">
+                      <i class="fab fa-facebook fa-2x"></i>
+                  </a>
+                                  <a href="https://twitter.com/cloudsconvert" class="btn btn-twitter btn-simple rounded social-shadow p-2" target="_blank">
+                      <i class="fab fa-twitter fa-2x"></i>
+                  </a>
+                                  <a href="https://www.instagram.com/cloudsconvert" class="btn btn-instagram btn-simple rounded social-shadow p-2" target="_blank">
+                      <i class="fab fa-instagram fa-2x"></i>
+                  </a>
+                                  <a href="https://t.me/cloudsconvert" class="btn btn-telegram btn-simple rounded social-shadow p-2" target="_blank">
+                      <i class="fab fa-telegram fa-2x"></i>
+                  </a>
+                            </div>
+          </div>
+                
+                  <hr class="horizontal dark mb-5">
+        
+        <div class="container">
+          <div class="row">
+                            
+                
+                        <div class="col-md-3 mb-3">
+                          <div>
+                            <h3 class="text-primary font-weight-bolder">About Us</h3>
+<p>CloudsConvert is your online web tools for file conversions. We support JPG, PNG, Word, PDF. Also, you can use YouTube Thumbnail Downloader, QR Code Generator without paying money.</p>
+                          </div>
+                        </div>
+                      
+                        <div class="col-md-2 col-sm-6 col-6 mb-3">
+                          <div>
+                            <h3 class="text-primary font-weight-bolder">Most Used Tools</h3>
+<ul class="flex-column ms-n3 nav">
+<li class="nav-item"><a class="nav-link ps-0" title="About" href="https://cloudsconvert.com/youtube-thumbnail-downloader">YouTube Thumbnail Downloader</a></li>
+<li><a class="nav-link ps-0" title="Update" href="https://cloudsconvert.com/qr-code-decoder">QR Code Decoder</a></li>
+<li><a class="nav-link ps-0" title="Bug" href="https://yt.cloudsconvert.com/">YouTube Video Downloader</a></li>
+<li class="nav-item"><a class="nav-link ps-0" title="FAQs" href="https://cloudsconvert.com/password-generator">Password Generator</a></li>
+<li><a class="nav-link ps-0" title="Report" href="https://cloudsconvert.com/image-converter">Image Converter</a></li>
+<li class="nav-item"><a class="nav-link ps-0" title="Contact" href="https://icongenerator.cloudsconvert.com/">Icon Generator</a></li>
+</ul>
+                          </div>
+                        </div>
+
+                        <div class="col-md-2 col-sm-6 col-6 mb-3">
+                          <div>
+                            <h3 class="text-primary font-weight-bolder">Support</h3>
+<ul class="flex-column ms-n3 nav">
+<li class="nav-item"><a class="nav-link ps-0" title="About" href="https://cloudsconvert.com/about">About</a></li>
+<li><a class="nav-link ps-0" title="Changelog" href="https://cloudsconvert.com/changelog">Changelog</a></li>
+<li><a class="nav-link ps-0" title="Bug" href="https://cloudsconvert.com/bug">Bug</a></li>
+<li class="nav-item"><a class="nav-link ps-0" title="FAQs" href="https://cloudsconvert.com/faqs">FAQs</a></li>
+<li><a class="nav-link ps-0" title="Report" href="https://cloudsconvert.com/report">Report</a></li>
+<li class="nav-item"><a class="nav-link ps-0" title="Contact" href="https://cloudsconvert.com/contact">Contact</a></li>
+</ul>
+                          </div>
+                        </div>
+
+                        <div class="col-md-2 col-sm-6 col-6 mb-3">
+                          <div>
+                            <h3 class="text-primary font-weight-bolder">Trending</h3>
+<ul class="flex-column ms-n3 nav">
+<li class="nav-item"><a class="nav-link ps-0" title="Blog" href="https://www.cloudsconvert.com/blog">Blog</a></li>
+</ul>
+                          </div>
+                        </div>
+
+                        <div class="col-md-2 col-sm-6 col-6 mb-3 me-auto">
+                          <div>
+                            <h3 class="text-primary font-weight-bolder">Legal</h3>
+<ul class="flex-column ms-n3 nav">
+<li class="nav-item"><a class="nav-link ps-0" title="Advertise" href="https://cloudsconvert.com/advertising">Advertise</a></li>
+<li class="nav-item"><a class="nav-link ps-0" title="Disclaimer" href="https://cloudsconvert.com/disclaimer">Disclaimer</a></li>
+<li class="nav-item"><a class="nav-link ps-0" title="Terms &amp; Conditions" href="https://cloudsconvert.com/terms-and-conditions">Terms &amp; Conditions</a></li>
+<li class="nav-item"><a class="nav-link ps-0" title="Privacy Policy" href="https://cloudsconvert.com/privacy">Privacy Policy</a></li>
+<li><a class="nav-link ps-0" title="Cookie Policy" href="https://cloudsconvert.com/cookie-policy">Cookie Policy</a></li>
+</ul>
+                          </div>
+                        </div>
+
+                                  
+              
+                          <div class="col-12">
+                <div class="text-center">
+                  <p class="my-4 text-sm">
+                    </p><p>Copyrights © 2022. All Rights Reserved by <a title="CloudsConvert" href="https://cloudsconvert.com/">CloudsConvert</a></p>                  <p></p>
+                </div>
+              </div>
+            
+          </div>
+        </div>
+      </footer>
